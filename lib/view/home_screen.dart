f3b77487fc6300216d13fb2cc966395cb3012cae
Sabiha <ssabiha104@gmail.com>
2024-09-13 10:40:19 -0700

    Initial commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:news_app225/model/news_channel_headlines_model.dart';
 import 'package:news_app225/view_model/news_view_model.dart';
 
@@ -14,10 +15,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   NewsViewModel newsViewModel = NewsViewModel();
+  final format = DateFormat('MMMM dd, yyyy');
 
   @override
   Widget build(BuildContext context) {
-    var media = MediaQuery.sizeOf(context);
+    var width = MediaQuery.sizeOf(context).width * 1;
+    var height = MediaQuery.sizeOf(context).height * 1;
 
     return Scaffold(
       appBar: AppBar(
@@ -28,85 +31,147 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Center(
           child: Text(
             "News",
-            style: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w700),
+            style:
+                GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w700),
           ),
         ),
       ),
       body: ListView(
         children: [
+          // Headline for the news section
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                "Top Headlines",
+                style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
           SizedBox(
-            height: media.height * 0.5,
-
-            child: FutureBuilder<NewsChannelHeadLinesModel>(
-              future: newsViewModel.fetchNewsChannelHeadlinesApi(),
-              builder: (BuildContext context, AsyncSnapshot<NewsChannelHeadLinesModel> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: SpinKitCircle(
-                      size: 50,
-                      color: Colors.blue,
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (snapshot.hasData && snapshot.data!.articles != null && snapshot.data!.articles!.isNotEmpty) {
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data!.articles!.length,
-                    itemBuilder: (context, index) {
-                      final article = snapshot.data!.articles![index];
-
-                      return Container(
-
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            // Background image
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20),
-                                child: CachedNetworkImage(
-                                  imageUrl: article.urlToImage ?? '',
-                                  fit: BoxFit.cover,
-                                  width: media.width,  // Ensure it takes full width
-                                  height: media.height * 0.9,
-                                  // Adjust height
-                                  placeholder: (context, url) => const SizedBox(
-                                    width: 50,
-                                    height: 50,
-                                    child: spinkit2,
-                                  ),
-                                  errorWidget: (context, url, error) => const Icon(
-                                    Icons.error_outline,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            // Overlaying text or content (optional)
-                            Positioned(
-                              bottom: 1,
-                              left: 5,
-                              child: Container(
-                                margin: EdgeInsets.all(8),
-                                height: media.height*0.03,
-                                color: Colors.red, // Add a background to make text readable
-                                child: Text(
-                                  article.title ?? 'No Title',
-                                  style: const TextStyle(fontSize:20,color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          ],
+            height: height * 0.55,
+            width: width,
+            child: Stack(
+              children: [
+                FutureBuilder<NewsChannelHeadLinesModel>(
+                  future: newsViewModel.fetchNewsChannelHeadlinesApi(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<NewsChannelHeadLinesModel> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: SpinKitCircle(
+                          color: Colors.blue,
+                          size: 50,
                         ),
                       );
-                    },
-                  );
-                } else {
-                  return const Center(child: Text('No data available'));
-                }
-              },
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Error: ${snapshot.error}'),
+                      );
+                    } else if (snapshot.hasData &&
+                        snapshot.data!.articles!.isNotEmpty) {
+                      return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: snapshot.data!.articles!.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: SizedBox(
+                              height: height * 0.9,
+                              width: width * 0.999,
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: CachedNetworkImage(
+                                        imageUrl: snapshot.data!
+                                                .articles![index].urlToImage ??
+                                            '',
+                                        fit: BoxFit.cover,
+                                        width: width,
+                                        height: height,
+                                        placeholder: (context, url) =>
+                                            Container(
+                                          alignment: Alignment.center,
+                                          child: spinkit2,
+                                        ),
+                                        errorWidget: (context, url, error) =>
+                                            const Icon(
+                                          Icons.error_outline,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 20, // Position the card 20 pixels from the bottom
+                                    left: 20,   // Optional: add padding to the left
+                                    right: 20,  // Optional: add padding to the right
+                                    child: Card(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      color: Colors.white.withOpacity(0.8), // Slightly transparent background
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(12.0), // Padding inside the card
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              snapshot.data!.articles![index].title.toString(), // Article title
+                                              maxLines: 2, // Limit to 2 lines
+                                              overflow: TextOverflow.ellipsis, // Add ellipsis for overflowing text
+                                              textAlign: TextAlign.center, // Center-align the text
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black, // Text color
+                                              ),
+                                            ),
+                                            const SizedBox(height: 8), // Spacing between title and source/date
+                                            Text(
+                                              snapshot.data!.articles![index].source!.name.toString(), // Article source
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w400,
+                                                color: Colors.grey[800], // Lighter text color for source
+                                              ),
+                                            ),
+                                            Text(
+                                              format.format(DateTime.parse(snapshot.data!.articles![index].publishedAt.toString())), // Article date
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w300,
+                                                color: Colors.blue[800], // Even lighter text color for date
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Center(
+                        child: Text('No data available'),
+                      );
+                    }
+                  },
+                ),
+              ],
             ),
           ),
         ],
